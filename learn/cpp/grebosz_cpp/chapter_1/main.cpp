061af45f9b4bee02d591fa4576b67286e285f335
Patryk Koniarczyk <koniarczykpatryk@gmail.com>
2025-09-11 15:36:48 -0500

    Add initial project files, C++ program for unit conversion and solve some excercises from a book

---
+++ learn/cpp/grebosz_cpp/chapter_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    double cal = 0;
+    double centimetre = 0;
+    std::cout << "witamy\nna\np\no\nkladzie" << std::endl;
+    std::cout << "Podaj liczbe w centymatrach: " << std::endl;
+    std::cin >> centimetre;
+    cal = centimetre / 2.54;
+    std::cout << centimetre << " cm to " << cal << " cali." << std::endl;
+
+    return 0;
+}
